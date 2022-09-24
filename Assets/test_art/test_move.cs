8bd4daa0c1fede69c8f3ce550051e2f94afbc3a4
D.Galga <ex.spake@gmail.com>
2022-09-24 13:45:23 -0500

    TestSprite
    Created, added, and configured test sprite for best appearance. Next step is to make sure it continues looking ok while moving.

---
+++ Assets/test_art/test_move.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_move : MonoBehaviour
+{
+    const float MOVESPEED = 20f;
+
+    void Update()
+    {
+        
+    }
+}
