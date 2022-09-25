ce2b3a59886580bf9f10ffbefdb2b46895a36f2c
D.Galga <ex.spake@gmail.com>
2022-09-24 22:09:37 -0500

    Created mine prefab object and modified collision boxes to be around feet
    Created mine prefab object and modified collision boxes to be around feet

---
+++ Assets/Scripts/ExplosiveMine.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveMine : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        
+    }
+}
