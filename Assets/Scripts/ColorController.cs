1f03b6b3499e7fc6cf69b543652edfb938764926
D.Galga <ex.spake@gmail.com>
2022-09-24 19:27:48 -0500

    Colors script
    Started colors script to get color codes converted to unity rgb from hex, and generally ensure they're written down.

---
+++ Assets/Scripts/ColorController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorController : MonoBehaviour
+{
+    //player color = (22,198,12) or (19,161,14)
+    [SerializeField] Color32 viewColor1 = new Color32(192,156,0,255);
+    [SerializeField] Color32 viewColor2 = new Color32(197,15,31,255);
+    [SerializeField] Color32 viewColor3 = new Color32(136,23,152,255);
+    [SerializeField] Color32 viewColor4 = new Color32(242,242,242,255);
+}
